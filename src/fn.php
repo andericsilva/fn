96f407fdb3abc12ee054ea496f8f7e5dee2d17dd
Anderson Ismael Couto da Silva <73040867+andericsilva@users.noreply.github.com>
2020-11-10 15:42:59 -0300

    first commit

---
+++ src/fn.php
@@ -0,0 +1,61 @@
+<?php
+$fns=null;
+function fn(...$params){
+    global $fn;
+    global $fns;
+    global $root;
+    if(isset($params[0])){
+        $name=$params[0];
+        unset($params[0]);
+        $fileName1=$root.'/fn/'.$name.'/'.$name.'.php';
+        $fileName2=$root.'/fn/'.$name.'.php';
+        $fileName3=$root.'/'.$name.'.php';
+    }else{
+        die('error:<br>empty $fn name');
+    }
+    if(file_exists($fileName1)){
+        $fileName=$fileName1;
+    }elseif(file_exists($fileName2)){
+        $fileName=$fileName2;
+    }else{
+        $fileName=$fileName3;
+    }
+    if(file_exists($fileName)){
+        if(@explode('/',$name)[0]=='view'){//caso seja view
+            $fn('intFunctions');//funções de tradução
+            //globais
+            $tempArr['fn']=$fn;//$fn sem prefixo global
+            $tempArr['cfg']=$fn("cfg");
+            extract($tempArr);
+            unset($tempArr);
+            if(@is_array($params[1])){
+                $params[1][]['data']=$params[1];//$params[1] = $data
+                extract($params[1]);//sobrescreve as variáveis globais
+            }
+            ob_start();
+            require $fileName;
+            $obj=ob_get_contents();
+            ob_end_clean();
+            print $obj;
+        }else{
+            //cache
+            if(isset($fns[$fileName])){
+                $obj=$fns[$fileName];//existe no cache
+            }else{
+                $obj=require $fileName;
+                $fns[$fileName]=$obj;//não existe no cache
+            }
+            if(count($params)>0){
+                return call_user_func_array($obj,$params);
+            }else{
+                if(is_object($obj)){
+                    return call_user_func($obj);
+                }else{
+                    return $obj;//caso a função não tenha parametros
+                }
+            }
+        }
+    }else{
+        die('error:<br>$fn '.$fileName.' not found');
+    }
+}
